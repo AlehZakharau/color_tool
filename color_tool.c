c66d160067f1fe01c8190547a6413c529482996f
Aleh Zaharau <38154177+AlehZakharau@users.noreply.github.com>
2023-12-04 14:34:35 -0500

    color convector

---
+++ color_tool.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct color{
+   double r;
+   double g;
+   double b;
+} color;
+
+void print_color(color *col){
+   printf("Color: <%.2lf,%.2lf,%.2lf>\n", col->r, col->g, col->b);
+}
+
+void check_range(double *c){
+   if(*c < 0.0)
+      *c = 0.0;
+   else if(*c > 255.0)
+      *c = 255.0;
+}
+
+void check_input_color(color *col){
+   check_range(&col->r);
+   check_range(&col->g);
+   check_range(&col->b);
+}
+
+color convert_color(color *col){
+   color col_out = { col->r / 255.0, col->g / 255.0, col->b / 255.0 };
+   return col_out;
+}
+
+int main(){
+   color col = {0.0, 0.0, 0.0};
+   printf("Enter red color in 0-255 range: ");
+   scanf("%lf", &col.r);
+   printf("Enter green color in 0-255 range: ");
+   scanf("%lf", &col.g);
+   printf("Enter blue color in 0-255 range: ");
+   scanf("%lf", &col.b);
+   check_input_color(&col);
+
+   printf("Input ");
+   print_color(&col);
+
+   color col_out = convert_color(&col);
+   printf("Output ");
+   print_color(&col_out);
+
+   return 0;
+}
