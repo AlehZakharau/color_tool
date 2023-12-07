bac16f419baeb78092a6a4ba6b77da0095443dfc
Aleh Zaharau <38154177+AlehZakharau@users.noreply.github.com>
2023-12-07 09:05:00 -0500

    cicly command

--- color_tool.c
+++ color_tool.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 
+#define BIG = 1
+#define SMALL = 2
+#define END = 0
+
 typedef struct color{
    double r;
    double g;
@@ -29,22 +33,50 @@ color convert_color(color *col){
    return col_out;
 }
 
-int main(){
-   color col = {0.0, 0.0, 0.0};
-   printf("Enter red color in 0-255 range: ");
-   scanf("%lf", &col.r);
-   printf("Enter green color in 0-255 range: ");
-   scanf("%lf", &col.g);
-   printf("Enter blue color in 0-255 range: ");
-   scanf("%lf", &col.b);
-   check_input_color(&col);
-
-   printf("Input ");
-   print_color(&col);
-
-   color col_out = convert_color(&col);
+void print_output(color *col){
+   color col_out = convert_color(col);
    printf("Output ");
    print_color(&col_out);
+   printf("\n:::\n\n");
+
+   //input_comman(col);
+}
+
+void input_big_range_color(color *col){
+   printf("Print red color in 0-255 range: ");
+   scanf("%lf", &col->r);
+   printf("Print green color in 0-255 range: ");
+   scanf("%lf", &col->g);
+   printf("Print blue color in 0-255 range: ");
+   scanf("%lf", &col->b);
+   check_input_color(col);
+
+   printf("\nInput ");
+   print_color(col);
+
+   print_output(col);
+}
+
+
+int input_command(color *col){
+   int command;
+   printf("Choose command \n\"BIG\" == 1 or \"SMALL\" == 2 ");
+   scanf("%d", &command);
+
+   if(command == 1){
+      input_big_range_color(col);
+   }
+   return command;
+}
+
+
+
+int main(){
+   color col = {0.0, 0.0, 0.0};
+   int command = input_command(&col);
+
+   if(command != 0)
+      input_command(&col);
 
    return 0;
 }
